51ae232ad50e528cab7b938acd3a94408948506b
howdysuraj <howdysuraj@gmail.com>
2023-09-27 16:38:03 +0530

    api

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const core = require('@actions/core');
 const exec = require("@actions/github");
 
+const jscpd = require('jscpd')
+
+const clones = jscpd(process.argv);
+
 async function run() {
     try {
-      core.info("Hello From Duplicate Logger")
+      core.info("Hello From Duplicate Logger",clones)
     } catch (error) {
         core.setFailed(error.message);
     }
