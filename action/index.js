b52b0cf3de5d188a1c72d4a9395e9483ce46370b
howdysuraj <howdysuraj@gmail.com>
2023-09-27 17:18:26 +0530

    fs

--- action/index.js
+++ action/index.js
@@ -86849,7 +86849,7 @@ const path = __nccwpck_require__(71017)
 //   report/jscpd-report.json
 async function run() {
     try {
-      filename = path.join(process.cwd(), 'report', 'jscpd-report.json');
+      let filename = path.join(process.cwd(), 'report', 'jscpd-report.json');
       const coverage = __nccwpck_require__(25387)(filename);
       console.log("coverage",coverage)
       core.info("Hello From Duplicate Logger", __dirname)
