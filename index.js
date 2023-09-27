b5137f88643926e0f20a85a9bb35122062438c2e
howdysuraj <howdysuraj@gmail.com>
2023-09-27 16:58:13 +0530

    cs

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const exec = require("@actions/github");
 import {jscpd} from 'jscpd';
 
 
-const clones = jscpd(process.argv);
-
+// const clones = jscpd(process.argv);
+const clones = await detectClones({
+    path: [
+      __dirname + '/../fixtures'
+    ],
+    silent: true
+  });
+  console.log(clones);
 async function run() {
     try {
       core.info("Hello From Duplicate Logger",clones)
