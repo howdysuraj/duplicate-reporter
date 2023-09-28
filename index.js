d1e31fba002f36da5ad68f9c1ec0b2c6af0ce6d5
howdysuraj <howdysuraj@gmail.com>
2023-09-28 14:52:21 +0530

    sd

--- index.js
+++ index.js
@@ -1,30 +1,43 @@
-const core = require('@actions/core');
-const fs = require('fs');
+const core = require("@actions/core");
+const fs = require("fs");
+const github = require("@actions/github")
+let octokit;
 
-async function generateBadges(report){
-    let string = ``;
-    for (const [key, value] of Object.entries(report.formats)) {
-        console.log(`${key}: ${value}`);
-        string += `\n ![${key}](https://img.shields.io/badge/${key}-${value.total.percentage}%25-lightgrey)`
-      }
-//    const badges = report.formats.map( type => {
-//     return ` ![${type}](https://img.shields.io/badge/${type}-${type.total.percentage}%25-${color}) `
-//    })
-   return string;
+
+
+async function generateBadges(report) {
+  let string = ``;
+  for (const [key, value] of Object.entries(report.formats)) {
+    console.log(`${key}: ${value}`);
+    string += `\n ![${key}](https://img.shields.io/badge/${key}-${value.total.percentage}%25-lightgrey)`;
+  }
+  return string;
 }
 
 async function run() {
-    try {
-      const filepath = core.getInput("path");
-      const data = fs.readFileSync(`${process.env.GITHUB_WORKSPACE}/${filepath}`, 'utf8');
-      const json = JSON.parse(data);
-      core.info(json.statistics)
-      const badgesString = await generateBadges(json.statistics)
-      core.info(badgesString)
-    } catch (error) {
-        core.setFailed(error.message);
-    }
+  try {
+    octokit = new github.getOctokit(core.getInput("token"));
+    const filepath = core.getInput("path");
+    const data = fs.readFileSync(
+      `${process.env.GITHUB_WORKSPACE}/${filepath}`,
+      "utf8"
+    );
+    const json = JSON.parse(data);
+    core.info(json.statistics);
+    const badgesString = await generateBadges(json.statistics);
+    core.info(badgesString);
+    appendBadgeToReadMe()
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
+async function appendBadgeToReadMe(){
+    const res = await octokit.request(`GET /repos/fylein/fyle-app/content/README.md`);
+    const { path, sha, content, encoding } = res.data;
+    const rawContent = Buffer.from(content, encoding).toString();
+    const startIndex = rawContent.indexOf("## Other Projects");
+    core.info(res)
+}
 
 run();
