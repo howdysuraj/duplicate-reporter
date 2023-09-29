4fc116732d5ce20bb6a31a05c53340c619299a7d
howdysuraj <howdysuraj@gmail.com>
2023-09-29 13:21:49 +0530

    ock

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const core = require("@actions/core");
 const fs = require("fs");
-const github = require("@actions/github")
+const github = require("@actions/github");
 let octokit;
+const {
+  createOrUpdateTextFile,
+} = require("@octokit/plugin-create-or-update-text-file");
 
+const MyOctokit = github.plugin(createOrUpdateTextFile);
 
 
 async function generateBadges(report) {
@@ -16,7 +20,8 @@ async function generateBadges(report) {
 
 async function run() {
   try {
-    octokit = new github.getOctokit(core.getInput("token"));
+   // octokit = new github.getOctokit(core.getInput("token"));
+    octokit = new MyOctokit({ auth: core.getInput("token") });
     const filepath = core.getInput("path");
     const data = fs.readFileSync(
       `${process.env.GITHUB_WORKSPACE}/${filepath}`,
@@ -24,46 +29,47 @@ async function run() {
     );
     const json = JSON.parse(data);
     const badgesString = await generateBadges(json.statistics);
-    appendBadgeToReadMe(badgesString)
+    appendBadgeToReadMe(badgesString);
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-async function appendBadgeToReadMe(badge){
-    // const res = await octokit.request(`GET /repos/fylein/fyle-app/README.md`);
+async function appendBadgeToReadMe(badge) {
+  // const res = await octokit.request(`GET /repos/fylein/fyle-app/README.md`);
 
-const res = await octokit.request('GET /repos/fylein/fyle-app/readme', {
-    owner: 'OWNER',
-    repo: 'REPO',
-    branch:'duplicate-logger',
+  const res = await octokit.request("GET /repos/fylein/fyle-app/readme", {
+    owner: "OWNER",
+    repo: "REPO",
+    branch: "duplicate-logger",
     headers: {
-      'X-GitHub-Api-Version': '2022-11-28'
-    }
-  })
-    const { path, sha, content, encoding } = res.data;
-    console.log("ssshshs",sha)
-    const rawContent = Buffer.from(content, encoding).toString();
-    const startIndex = rawContent.indexOf("### Code Duplication Stats in app-v2");
-    const updatedContent = `${startIndex === -1 ? rawContent : rawContent.slice(0, startIndex)}\n${badge}`;
-    commitNewReadme( path, sha, encoding, updatedContent);
-};
-
-async function commitNewReadme( path, sha, encoding, updatedContent){
-    try {
-		await octokit.request(`PUT /repos/fylein/fyle-app/contents/README.md`, {
-			message: "Update README",
-			content: Buffer.from(updatedContent, "utf-8").toString(encoding),
-			path,
-			sha,
-            owner:'fylein',
-            repo:'fyle-app',
-            branch:'duplicate-logger'
-		});
-	} catch (err) {
-		console.log(err);
-	}
+      "X-GitHub-Api-Version": "2022-11-28",
+    },
+  });
+  const { path, sha, content, encoding } = res.data;
+  console.log("ssshshs", sha);
+  const rawContent = Buffer.from(content, encoding).toString();
+  const startIndex = rawContent.indexOf("### Code Duplication Stats in app-v2");
+  const updatedContent = `${
+    startIndex === -1 ? rawContent : rawContent.slice(0, startIndex)
+  }\n${badge}`;
+  commitNewReadme(path, sha, encoding, updatedContent);
 }
 
+async function commitNewReadme(path, sha, encoding, updatedContent) {
+  try {
+    await octokit.request(`PUT /repos/fylein/fyle-app/contents/README.md`, {
+      message: "Update README",
+      content: Buffer.from(updatedContent, "utf-8").toString(encoding),
+      path,
+      sha,
+      owner: "fylein",
+      repo: "fyle-app",
+      branch: "duplicate-logger",
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 run();
